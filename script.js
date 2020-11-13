a1ac6e557d10b27419a099c2304c4e85a486adf4
kipuchmeb <70568582+kipuchmeb@users.noreply.github.com>
2020-11-13 21:30:09 +0300

    Add files via upload
    commit2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function a(){
+   let random = ['Уедешь в Южную Корею', 'Появятся много денег']
    let out = document.querySelector('.out');
    console.log(out);
-   out.innerHTML = "gfgg";
+   out.innerHTML = random[Math.floor(Math.random() * random.length)];
+
+   
+
+
 }
